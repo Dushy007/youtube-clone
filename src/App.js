f08fbce799eb973e772018a49a0750286c51e0a8
dushyanth <dushyrevnant@gmail.com>
2021-05-16 12:30:36 +0530

    Added search Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import './App.css';
-import Header from './Header';
-import RecommendedVideos from './RecommendedVideos';
-import Sidebar from './Sidebar';
+import "./App.css";
+import Header from "./Header";
+import RecommendedVideos from "./RecommendedVideos";
+import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <div className="app__main">
-        <Sidebar/>
-        <RecommendedVideos/>
-      </div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <div className="app__main">
+              <Sidebar />
+              <SearchPage />
+            </div>
+          </Route>
+          <Route path="/">
+            <div className="app__main">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
